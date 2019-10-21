bf8f75c0297396dac95648b41fc0b384388b3f0d
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2019-10-21 05:10:49 +0000

    build: Updating mediawiki/mediawiki-codesniffer to 28.0.0
    The following sniffs are failing and were disabled:
    * PSR12.Properties.ConstantVisibility.NotFound
    
    Change-Id: I34f9289c5a9a24f8f9c7e14bede5f7b351f4adc0

--- tests/phpunit/GraphVizTest.php
+++ tests/phpunit/GraphVizTest.php
@@ -17,7 +17,7 @@ use WikiPage;
  */
 class GraphVizTest extends MediaWikiTestCase {
 
-	protected function setUp() {
+	protected function setUp() : void {
 		parent::setUp();
 		$this->setMwGlobals( 'wgEnableUploads', true );
 	}
